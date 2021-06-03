4e1ff789a5c19e44739680c0311db04de167a311
Mac <yurii.kyrylov.triare@gmail.com>
2021-06-03 16:28:56 +0300

    added base serven on expres

--- nodeExpress/index.js
+++ nodeExpress/index.js
@@ -1,8 +1,17 @@
-const express =require('express')
-
+const express = require('express')
+const patch = require('path')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.sendFile(patch.join(__dirname, 'views', 'index.html'))
+})
+
+
+app.get('/about', (req, res) => {
+    res.sendFile(patch.join(__dirname, 'views', 'about.html'))
+})
+
 const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running ${PORT}`)
 })
